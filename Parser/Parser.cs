e5a37279dd6482486e42e20c763afef9fe45e778
licarigianluca <licarigianluca@gmail.com>
2014-06-15 11:23:09 +0200

    primo

---
+++ Parser/Parser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Parser
+{
+    private Tokenizer t;
+    Token lookahead;
+
+    public Parser()
+    {
+
+    }
+
+    public void parse(String s)
+    {
+        t = new Tokenizer(s);
+        do
+        {
+            lookahead = t.nextToken();
+            Console.WriteLine(lookahead.Value);
+        }
+        while (lookahead.Type != (int)type.EOF);
+        Console.WriteLine("OK");
+    }
+}
+
---
+++ Parser/test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Test
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Per adesso non fa niente vero");
+        String expr = "25+3*5";
+        Parser p = new Parser();
+        p.parse(expr);
+    }
+}
