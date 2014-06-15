aa30c0cabd380c427ecf609b5655bfbe37832f71
licarigianluca <licarigianluca@gmail.com>
2014-06-15 11:36:14 +0200

    ora si

--- Parser/test.cs
+++ Parser/test.cs
@@ -3,7 +3,7 @@ class Test
 {
     static void Main(String[] args)
     {
-        Console.WriteLine("Per adesso non fa niente ");
+        Console.WriteLine("Per adesso  ");
         String expr = "25+3*5";
         Parser p = new Parser();
         p.parse(expr);
